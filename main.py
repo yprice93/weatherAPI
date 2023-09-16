6cb5dce494bc418152ce7eae27de862ef8f89903
Yelin Price <yelin.lee.1009@gmail.com>
2023-09-16 13:24:42 -0500

    build a restAPI

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+# @app.route("/api/vi/<station>/<date>")
+# def hi(station, date):
+#     return {"station": station, "date":date}
+
+@app.route("/api/vi/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+if __name__ == "__main__":
+    app.run(debug=True)
